19dd5a2097374c0e3b08890d94c31d8eab95be7e
Yair0 <yairyolotl@gmail.com>
2023-08-14 14:23:41 -0600

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes/cars.route");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
